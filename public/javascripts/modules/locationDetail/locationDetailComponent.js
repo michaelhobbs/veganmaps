9a2d36b3c842b05c10d3a56c34277b5306cd13d2
Michael Hobbs <michael@hobbs.im>
2016-12-17 19:20:25 +0100

    Navigation across locations from the detail view now follows the orderBy selected in the list view.

--- public/javascripts/modules/locationDetail/locationDetailComponent.js
+++ public/javascripts/modules/locationDetail/locationDetailComponent.js
@@ -5,8 +5,8 @@ angular.
     bindings: {
       locationDetails: '<'
     },
-    controller: ['$routeParams', '$http', 'LocationService',
-      function locationDetailController($routeParams, $http, LocationService) {
+    controller: ['$routeParams', '$http', '$filter', 'LocationService',
+      function locationDetailController($routeParams, $http, $filter, LocationService) {
         var ctrl = this;
         ctrl.locationId = $routeParams.locationId;
         ctrl.LocationService = LocationService;
@@ -22,7 +22,9 @@ angular.
             return (ctrl.LocationService.lastSearch.filterProp === 'all' || value[ctrl.LocationService.lastSearch.filterProp] === true);
           };
 
-          ctrl.filteredLocations = ctrl.LocationService.lastSearch.results.filter(ctrl.filterExpression);
+//TODO : move filter/orderBy logic to a service
+          ctrl.filteredLocations = $filter('filter')(ctrl.LocationService.lastSearch.results, ctrl.filterExpression);
+          ctrl.filteredLocations = $filter('orderBy')(ctrl.filteredLocations, ctrl.LocationService.lastSearch.orderProp);
           var index = ctrl.filteredLocations.findIndex(x => x._id === ctrl.locationId);
           // prepare next/previous links so user can navigate through search results without having to go back to map/list view
           if (index > 0) {
