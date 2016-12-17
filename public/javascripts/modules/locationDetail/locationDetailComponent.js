b35f03e87759cdd3ee31fd991ac0fd48a244697c
Michael Hobbs <michael@hobbs.im>
2016-12-17 17:03:02 +0100

    Basic restaurant detail view dumps the location object to screen.

--- public/javascripts/modules/locationDetail/locationDetailComponent.js
+++ public/javascripts/modules/locationDetail/locationDetailComponent.js
@@ -10,8 +10,9 @@ angular.
         var ctrl = this;
         ctrl.locationId = $routeParams.locationId;
         ctrl.LocationService = LocationService;
+        ctrl.fromSearch = ctrl.LocationService.lastSearch && ctrl.LocationService.lastSearch.results;
 
-        if (ctrl.LocationService.lastSearch && ctrl.LocationService.lastSearch.results) {
+        if (ctrl.fromSearch) {
           ctrl.locationDetails = ctrl.LocationService.lastSearch.results.find(function(place) {
             return (place._id === ctrl.locationId);
           });
