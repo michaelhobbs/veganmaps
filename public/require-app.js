5f76234dd91b620a46bffd8c6e3b76d0c93e4263
Michael Hobbs <michael@hobbs.im>
2017-04-11 21:15:23 +0200

    Add fetch to support fetch in safari.

--- public/require-app.js
+++ public/require-app.js
@@ -16,7 +16,8 @@
           text : '../lib/text/text',
           socketio : '../socket.io/socket.io',
           leaflet : '../lib/leaflet/dist/leaflet',
-          leafletgeosearch: '../node/leaflet-geosearch/dist/bundle.min'
+          leafletgeosearch: '../node/leaflet-geosearch/dist/bundle.min',
+          'whatwg-fetch' : '../lib/fetch/fetch'
           // es6: '../lib/requirejs-babel/es6', // added when trying to use bower's leaflet-geosearch, in order to transcompile at load time
           // babel: '../lib/requirejs-babel/babel-5.8.34.min'
       },
@@ -40,15 +41,17 @@
 
   // Start loading the main app file. Put all of
   // your application logic in there.
-  requirejs(['app'], function() {
-    console.log('requireJS loading success');
-  }, function(exception) {
-    console.log('requireJS loading error: ', exception.message);
-    if (exception.requireModules) {
-      console.log('requireModules: ', exception.requireModules);
-    }
-    if (exception.stack) {
-      console.log(exception.stack);
-    }
-    throw exception.message;
+  require(['whatwg-fetch'], function () {
+    requirejs(['app'], function() {
+      console.log('requireJS loading success');
+    }, function(exception) {
+      console.log('requireJS loading error: ', exception.message);
+      if (exception.requireModules) {
+        console.log('requireModules: ', exception.requireModules);
+      }
+      if (exception.stack) {
+        console.log(exception.stack);
+      }
+      throw exception.message;
+    });
   });
